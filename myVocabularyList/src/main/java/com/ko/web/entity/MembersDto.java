b02cbf8691d10df1603aaf5c35a784a8cd12feb5
kokoko12334 <gnjgnjgnj1@gmail.com>
2023-04-05 01:18:10 +0900

    myvocab1

---
+++ myVocabularyList/src/main/java/com/ko/web/entity/MembersDto.java
@@ -0,0 +1,137 @@
+package com.ko.web.entity;
+
+import java.util.Date;
+
+public class MembersDto {
+
+	
+	private String id;
+	private String pwd;
+	private String name;
+	private String gender;
+	private String birthday;
+	private String phone;
+	private Date regdate;
+	private String email;
+	
+	
+	
+	public MembersDto() {
+		
+	}
+
+
+
+	public MembersDto(String id, String pwd, String name, String gender, String birthday, String phone, Date regdate,
+			String email) {
+		super();
+		this.id = id;
+		this.pwd = pwd;
+		this.name = name;
+		this.gender = gender;
+		this.birthday = birthday;
+		this.phone = phone;
+		this.regdate = regdate;
+		this.email = email;
+	}
+
+
+
+	public String getId() {
+		return id;
+	}
+
+
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+
+
+	public String getPwd() {
+		return pwd;
+	}
+
+
+
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+
+
+
+	public String getName() {
+		return name;
+	}
+
+
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+
+
+	public String getGender() {
+		return gender;
+	}
+
+
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+
+
+	public String getBirthday() {
+		return birthday;
+	}
+
+
+
+	public void setBirthday(String birthday) {
+		this.birthday = birthday;
+	}
+
+
+
+	public String getPhone() {
+		return phone;
+	}
+
+
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+
+
+	public Date getRegdate() {
+		return regdate;
+	}
+
+
+
+	public void setRegdate(Date regdate) {
+		this.regdate = regdate;
+	}
+
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+	
+	
+}
---
+++ myVocabularyList/src/main/webapp/WEB-INF/views/myvocabulary/mynotes.jsp
@@ -0,0 +1,26 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+    
+<%@ taglib prefix="c" uri = "http://java.sun.com/jsp/jstl/core" %>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+	<p>${id}님의 단어장</p> 
+	<form action = "/logout" method = "post">
+		<input type = submit value = "로그아웃">
+	</form>
+	
+	
+	
+	
+	
+	
+	
+</body>
+</html>
